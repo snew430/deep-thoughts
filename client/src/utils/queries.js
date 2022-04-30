207b03b91263785ee58f8bec98c38073c3cf5d49
snew430 <seanpnew@hotmail.com>
2022-04-30 14:49:45 -0400

    added friendlist

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -4,6 +4,7 @@ export const QUERY_THOUGHTS = gql`
   query thoughts($username: String) {
     thoughts(username: $username) {
       _id
+      username
       thoughtText
       createdAt
       reactionCount
@@ -12,6 +13,45 @@ export const QUERY_THOUGHTS = gql`
         createdAt
         username
         reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
       }
     }
   }
