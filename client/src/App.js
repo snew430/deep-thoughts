65c41d8156a056ee4643b8c00ab9f496e95699de
snew430 <seanpnew@hotmail.com>
2022-05-01 06:32:51 -0400

    added login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import {
   InMemoryCache,
   createHttpLink,
 } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -19,8 +20,18 @@ const httpLink = createHttpLink({
   uri: "/graphql",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -35,7 +46,7 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              {/* <Route path="/profile" element={<Profile />} /> */}
+              <Route path="/profile" element={<Profile />} />
               {/* <Route path="/thought" element={<SingleThought />} /> */}
               <Route path="/profile/:username" element={<Profile />} />
               <Route path="/thought/:id" element={<SingleThought />} />
